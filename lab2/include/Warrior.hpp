aafb89c8157e618d62a3dc09732079f18cc08baf
Pasha <pashok190408@gmail.com>
2026-03-26 21:10:36 +0200

    lab5: update Warrior class (implement interact override)

--- lab2/include/Warrior.hpp
+++ lab2/include/Warrior.hpp
@@ -8,8 +8,7 @@ private:
 
 public:
     Warrior();
-    Warrior(std::string name, int hp, int attack, int level, int armor);
-
+    Warrior(const std::string& name, int hp, int attack, int level, int armor);
     Warrior(const Warrior& other);
     Warrior(Warrior&& other) noexcept;
 
@@ -19,6 +18,7 @@ public:
     void setArmor(int armor);
 
     std::string info() const override;
+    void interact() const override; // interact
 
     Warrior& operator=(const Warrior& other);
     Warrior& operator=(Warrior&& other) noexcept;
--- lab2/src/Warrior.cpp
+++ lab2/src/Warrior.cpp
@@ -2,21 +2,22 @@
 #include <sstream>
 #include <stdexcept>
 #include <utility>
+#include <iostream>
 
 Warrior::Warrior() : Warrior("DefaultWarrior", 150, 30, 1, 20) {}
 
-Warrior::Warrior(std::string name, int hp, int attack, int level, int armor)
-    : Player(std::move(name), hp, attack, level), armor_(armor)
+Warrior::Warrior(const std::string& name, int hp, int attack, int level, int armor)
+    : Player(name, hp, attack, level), armor_(armor)
 {
     if (armor_ < 0) {
         throw std::invalid_argument("Armor cannot be negative");
     }
 }
 
-Warrior::Warrior(const Warrior& other)
+Warrior::Warrior(const Warrior& other) // copy constructor
     : Player(other), armor_(other.armor_) {}
 
-Warrior::Warrior(Warrior&& other) noexcept
+Warrior::Warrior(Warrior&& other) noexcept // move constructor
     : Player(std::move(other)), armor_(other.armor_)
 {
     other.armor_ = 0;
@@ -44,6 +45,11 @@ std::string Warrior::info() const {
         << ", armor=" << armor_
         << "}";
     return oss.str();
+}
+
+void Warrior::interact() const { // interact
+    std::cout << "Warrior " << name()
+              << " prepares for battle! (armor: " << armor_ << ")\n";
 }
 
 Warrior& Warrior::operator=(const Warrior& other) {
